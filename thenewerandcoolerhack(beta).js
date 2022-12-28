5c554aa97bb1e574f3503774ba61bb1cb48eafc6
non-reai <63729314+non-reai@users.noreply.github.com>
2022-12-28 00:26:31 -0500

    Update thenewerandcoolerhack(beta).js

--- thenewerandcoolerhack(beta).js
+++ thenewerandcoolerhack(beta).js
@@ -1,7 +1,5 @@
 const sockets = [];
 var nativeWebSocket = window.WebSocket;
-var started = false;
-var otherStart = false;
 var lastDataSent = null;
 var using = null;
 
@@ -24,34 +22,6 @@ document.onkeydown = (e)=>{
 	} else if (e.key == "u") {
 		sockets[0].send(using);
 	};
-};
-const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
-	const byteCharacters = atob(b64Data);
-	const byteArrays = [];
-
-	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-		const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-		const byteNumbers = new Array(slice.length);
-		for (let i = 0; i < slice.length; i++) {
-			byteNumbers[i] = slice.charCodeAt(i);
-		}
-
-		const byteArray = new Uint8Array(byteNumbers);
-		byteArrays.push(byteArray);
-	}
-
-	const blob = new Blob(byteArrays, { type: contentType });
-	return blob;
-};
-function arrayBufferToB64(buffer) {
-	var binary = '';
-	var bytes = new Uint8Array(buffer);
-	var len = bytes.byteLength;
-	for (var i = 0; i < len; i++) {
-		binary += String.fromCharCode(bytes[i]);
-	};
-	return window.btoa(binary);
 };
 window.onerror = (err)=>{
 	alert(err);
